2f9d518d4a73e7860ad4158270bc6cfa0a405fc5
jhlarsen1 <jhlarsen@live.com>
2024-09-19 10:31:03 -0400

    first upload

---
+++ P1LAB1_LarsenJustin.py
@@ -0,0 +1,13 @@
+# Justin Larsen
+# 9/17/2024
+# P1LAB1
+# Get Input from user and display output
+
+# Get first name from user
+first_name = input("Enter your first name: ")
+
+#Get last name from the user
+last_name = input("Enter your last name: ")
+
+#Print Info to user
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
